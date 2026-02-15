1bd0c3d03e02ebfc0efcc2de4664ac1bacda8a82
vishalchadithya <vishalchadithya88@gmail.com>
2026-02-15 13:11:30 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,44 @@
-let count = 0;
-
-function addToCart() {
-    count++;
-    document.getElementById("cart-count").innerText = count;
-    alert("Item added to cart!");
-}
-
-function scrollToProducts() {
-    document.getElementById("products").scrollIntoView({
-        behavior: "smooth"
-    });
-}
+const products = [
+    {
+        id: 1,
+        name: "Black Street Tee",
+        price: 50.00,
+        category: "T-Shirts",
+        image: "images/black-tee.jpg"
+    },
+    {
+        id: 2,
+        name: "Urban Hoodie",
+        price: 85.00,
+        category: "Hoodies",
+        image: "images/urban-hoodie.jpg"
+    },
+    {
+        id: 3,
+        name: "Monochrome Jacket",
+        price: 120.00,
+        category: "Jackets",
+        image: "images/monochrome-jacket.jpg"
+    },
+    {
+        id: 4,
+        name: "White Minimal Tee",
+        price: 40.00,
+        category: "T-Shirts",
+        image: "images/white-tee.jpg"
+    },
+    {
+        id: 5,
+        name: "Street Puffer",
+        price: 150.00,
+        category: "Jackets",
+        image: "images/street-puffer.jpg"
+    },
+    {
+        id: 6,
+        name: "Logo Hoodie",
+        price: 95.00,
+        category: "Hoodies",
+        image: "images/logo-hoodie.jpg"
+    }
+];
